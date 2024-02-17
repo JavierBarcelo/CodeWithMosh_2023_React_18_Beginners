b388295217951f09265b542b776bf0f136cb16fd
Javier B <javi.barcelo@hotmail.com>
2024-02-17 14:25:37 +0000

    Section 03 - 05 - Props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,12 @@
-import { MouseEvent } from "react";
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
 
+function ListGroup({ items, heading, onSelectItem }: Props) {
   // hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -11,12 +14,9 @@ function ListGroup() {
     return items.length === 0 && <p>No item found</p>;
   };
 
-  // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
-
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {getMessage()}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -28,7 +28,7 @@ function ListGroup() {
                 : "list-group-item"
             }
             onClick={() => {
-              handleClick;
+              onSelectItem(item);
               setSelectedIndex(index);
             }}
           >
